edd31929e144df3c827cadf6599eeedc28e8d216
ChibisovAS <71130890+ChibisovAS@users.noreply.github.com>
2021-03-22 18:11:04 +0300

    Update AbstractDAO.java

--- src/main/java/com/github/chibisovas/stb/DAO/AbstractDAO.java
+++ src/main/java/com/github/chibisovas/stb/DAO/AbstractDAO.java
@@ -5,10 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public abstract class AbstractDAO {
-
+    
     private static final String URL = "jdbc:mysql://localhost:3306/java_rush_news";
-    private static final String USERNAME ="root";
-    private static final String PASSWORD = "Overmight_4140";
+    private static final String USERNAME ="****";
+    private static final String PASSWORD = "****";
 
     protected static Connection connection;
 
